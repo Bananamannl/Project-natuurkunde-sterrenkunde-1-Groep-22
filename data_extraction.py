3fe75cc6f41ad8967e7a28ca9385a36a1261bbbe
Merijn Post <merijnpost2006@gmail.com>
2026-06-02 17:40:41 +0200

    HoQI Data extraction code
    This file contains a function which can be called to extract the data from one of the HoQI files and output them as a dictionary of lists (See function description in code for more details)

---
+++ data_extraction.py
@@ -0,0 +1,39 @@
+## Function itself
+def Data_Extract(name):
+    """
+    Data_Extract( str ) -> dictionairy
+    This function takes the name (or path) of a raw HoQI data file and extracts the collumns of this file as lists embeded in a dictionairy, using the headers of columns as keys for the lists that are made up of the values of said columns
+    """
+    with open(name, 'r') as file:
+        column_line = file.readline()
+        column_names = column_line.split()
+        column_names.pop(0)
+
+        dictionairy_columns = {column: [] for column in column_names}
+        next(file)
+
+        for line in file:
+            line_split = line.split()
+            for i in range(0,len(line_split)):
+                 dictionairy_columns[column_names[i]].append(float(line_split[i]))
+    
+    return dictionairy_columns
+
+## Testing code of the function in abstraction
+with open('20260421_HoQIs.txt', 'r') as file:
+        column_line = file.readline()
+        column_names = column_line.split()
+        column_names.pop(0)
+
+        dictionairy_columns = {column: [] for column in column_names}
+        next(file)
+
+        for line in file:
+            line_split = line.split()
+            for i in range(0,len(line_split)):
+                 dictionairy_columns[column_names[i]].append(float(line_split[i]))
+        
+        ## Testing print command
+        ## print(dictionairy_columns[column_names[0]])
+        
+# %%
