6e7a1a9c697fd9ac7c25b717085bdf7c34049774
Timo <timoboomsma2007@gmail.com>
2026-06-04 10:44:14 +0200

    Corrected the braces in the file names and finished Fourier_transform
    I updated the file names and I worked a little more on the fourier transformation file

---
+++ Fourier_transform_9_10.py
@@ -0,0 +1,25 @@
+import numpy as np
+from gwpy.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+
+def get_asd(data, fs, segment_time):
+    """
+    Maak van een lijst met displacements een amplitude spectral density grafiek.
+    get_asd(datalijst, samplerate, duur van de meting in sec) 
+    """
+    data = np.asarray(data)
+    data = data -np.mean(data) #trek het gemiddelde eraf om te voorkomen dat je een ziek grote piek rond 0 Hz krijgt
+    ts = TimeSeries(data, sample_rate= fs)
+    ASD = ts.asd(seconds=segment_time)
+    return ASD
+
+#Vervolgens kun je de asd op de volgende manier plotten
+asd = get_asd(lijst, fs=1000, segment_time=10)
+
+plt.figure()
+plt.loglog(asd.frequencies.value, asd.value)
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("ASD [displacement]")
+plt.grid(True, which="both")
+plt.show()
