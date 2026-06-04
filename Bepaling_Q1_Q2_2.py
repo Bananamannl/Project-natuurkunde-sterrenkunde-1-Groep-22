f1c114038ebbe16c86dd990e46ab4d643a02480f
Merijn Post <merijnpost2006@gmail.com>
2026-06-04 11:49:26 +0200

    Test Arctan_Q1_Q2
    Added a testing part to the arctan file, and added all pycache to the gitignore file

--- Bepaling_Q1_Q2_2.py
+++ Bepaling_Q1_Q2_2.py
@@ -1,6 +1,38 @@
 import numpy as np
+from data_extraction_1 import Data_Extract
+
+data_20260421 = Data_Extract('20260421_HoQIs.txt')
+tijd = data_20260421["T"]
+
+PD2_1x = data_20260421["SENS_HOQI_1_H_INP_COS_IN"]
+PD3_1x = data_20260421["SENS_HOQI_1_H_INP_MCOS_IN"]
+PD1_1x = data_20260421["SENS_HOQI_1_H_INP_SIN_IN"] 
+PD2_2x = data_20260421["SENS_HOQI_2_H_INP_COS_IN"]
+PD3_2x = data_20260421["SENS_HOQI_2_H_INP_MCOS_IN"]
+PD1_2x = data_20260421["SENS_HOQI_2_H_INP_SIN_IN"]
+PD2_3x = data_20260421["SENS_HOQI_3_H_INP_COS_IN"]
+PD3_3x = data_20260421["SENS_HOQI_3_H_INP_MCOS_IN"]
+PD1_3x = data_20260421["SENS_HOQI_3_H_INP_SIN_IN"]
+PD1_1z = data_20260421["SENS_HOQI_1_V_INP_SIN_IN"]
+PD2_1z = data_20260421["SENS_HOQI_1_V_INP_COS_IN"]
+PD3_1z = data_20260421["SENS_HOQI_1_V_INP_MCOS_IN"]
+PD1_2z = data_20260421["SENS_HOQI_2_V_INP_SIN_IN"]
+PD2_2z = data_20260421["SENS_HOQI_2_V_INP_COS_IN"]
+PD3_2z = data_20260421["SENS_HOQI_2_V_INP_MCOS_IN"]
+PD1_3z = data_20260421["SENS_HOQI_3_V_INP_SIN_IN"]
+PD2_3z = data_20260421["SENS_HOQI_3_V_INP_COS_IN"]
+PD3_3z = data_20260421["SENS_HOQI_3_V_INP_MCOS_IN"]
+
 # functie om uit PD1, PD2 en PD3 een lijst te maken Q1 en een lijst Q2.
-def bepaling_Q1_Q2(PD1, PD2, PD3):  
+def Bepaling_Q1_Q2(PD1, PD2, PD3):  
     Q1 = np.array(PD1)-np.array(PD2)
     Q2 = np.array(PD1)-np.array(PD3)
     return Q1, Q2
+
+Q1_1x, Q2_1x = Bepaling_Q1_Q2(PD1_1x, PD2_1x, PD3_1x)
+Q1_2x, Q2_2x = Bepaling_Q1_Q2(PD1_2x, PD2_2x, PD3_2x)
+Q1_3x, Q2_3x = Bepaling_Q1_Q2(PD1_3x, PD2_3x, PD3_3x)
+Q1_1z, Q2_1z = Bepaling_Q1_Q2(PD1_1z, PD2_1z, PD3_1z)
+Q1_2z, Q2_2z = Bepaling_Q1_Q2(PD1_2z, PD2_2z, PD3_2z)
+Q1_3z, Q2_3z = Bepaling_Q1_Q2(PD1_3z, PD2_3z, PD3_3z)
+
---
+++ arctan_Q1_Q2_5_6_7.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib as mt
+
+from data_extraction_1 import *
+from bepaling_Q1_Q2_2 import *
+
+
+def Q1_Q2_Length(Q1, Q2):
+    """
+    Q1_Q2_Length( np.array, np.array ) - > np.array
+    This function takes two lists of Q1 and Q2 made from the HoQI data, and calculates the optical phase. It then uses the optical phase to calculate the measured length at each timestep. This is then outputed as another np.array.
+    """
+    w_length = 1064e-9 # The wavelength of the HoQI laser is 1064 nm
+
+    opt_phase = np.arctan2(Q1, Q2)
+
+    length = (opt_phase * w_length)/(4*np.pi)
+
+    return length
+
+## Testing the function
+HoQI_data = Data_Extract('20260421_HoQIs.txt') 
+
+# First HoQI PD1, PD2, PD3: 
+# SENS_HOQI_1_H_INP_SIN_IN SENS_HOQI_1_H_INP_COS_IN SENS_HOQI_1_H_INP_MCOS_IN
+HoQI_1_Q1, HoQI_1_Q2 = Bepaling_Q1_Q2(HoQI_data['SENS_HOQI_1_H_INP_SIN_IN'], HoQI_data['SENS_HOQI_1_H_INP_COS_IN'], HoQI_data['SENS_HOQI_1_H_INP_MCOS_IN'])
+
+displacement_list_HoQI_1 = Q1_Q2_Length(HoQI_1_Q1, HoQI_1_Q2)*10**9
+
+#print(HoQI_data['SENS_HOQI_1_H_INP_SIN_IN'])
+print(displacement_list_HoQI_1[0]-displacement_list_HoQI_1[1])
+print(HoQI_data['SENS_HOQI_1_H_DISP_LEN'][0]-HoQI_data['SENS_HOQI_1_H_DISP_LEN'][1])
