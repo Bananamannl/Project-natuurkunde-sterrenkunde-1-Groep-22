9db89aa319ace2c29f387f57ce8cc27b477f438a
KesseDonders <kessedonders@gmail.com>
2026-06-04 14:44:20 +0200

    Fixed the merging issue
    I have removed the parts that got doubled after the merging, so that there's no unnecessary code in the file.

--- bepaling_Q1_Q2_2.py
+++ bepaling_Q1_Q2_2.py
@@ -40,12 +40,6 @@ def Q_lijst():
     Q_lijst = [Q1_1x, Q2_1x, Q1_2x, Q2_2x, Q1_3x, Q2_3x, Q1_1z, Q2_1z, Q1_2z, Q2_2z, Q1_3z, Q2_3z]
     return Q_lijst
 
-Q1_2x, Q2_2x = bepaling_Q1_Q2(PD1_2x, PD2_2x, PD3_2x)
-Q1_3x, Q2_3x = bepaling_Q1_Q2(PD1_3x, PD2_3x, PD3_3x)
-Q1_1z, Q2_1z = bepaling_Q1_Q2(PD1_1z, PD2_1z, PD3_1z)
-Q1_2z, Q2_2z = bepaling_Q1_Q2(PD1_2z, PD2_2z, PD3_2z)
-Q1_3z, Q2_3z = bepaling_Q1_Q2(PD1_3z, PD2_3z, PD3_3z)
-
 #sla de eerste 3 op zodat we ze niet steeds hoeven op te halen
 Q1, Q2 = bepaling_Q1_Q2(PD1_1x, PD2_1x, PD3_1x)
 Q1_kopie = Q1.copy()
@@ -53,4 +47,3 @@ Q2_kopie = Q2.copy()
 
 np.save("Q1.npy", Q1_kopie)
 np.save("Q2.npy", Q2_kopie)
-
