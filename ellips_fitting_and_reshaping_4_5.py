eba59e65e866b27dd113006c557aecb42bd72de2
Timo <timoboomsma2007@gmail.com>
2026-06-04 16:15:02 +0200

    Code for reshaping the ellips and test plotting

--- ellips_fitting_and_reshaping_4_5.py
+++ ellips_fitting_and_reshaping_4_5.py
@@ -1,46 +1,11 @@
 import numpy as np
 from scipy.optimize import least_squares
-import matplotlib.pyplot as plt
 
-
-#Zorg dat Q1 en Q2 numpy arrays zijn
-#eerst het de modelfunctie definieren
 def residuals(params, x, y):
     x0, y0, a, b, theta = params
     xp = (x - x0) * np.cos(theta) + (y - y0) * np.sin(theta)
-    yp = (x - x0) * np.sin(theta) + (y - y0) * np.cos(theta)
+    yp = - (x - x0) * np.sin(theta) + (y - y0) * np.cos(theta)
     return xp ** 2 / a ** 2 + yp ** 2 / b ** 2 - 1
-
-results = least_squares(
-    residuals, #we fitten de modelfunctie
-    x0 = [0, 0, 1, 1, 0], #dit zijn de startcondities voor de fit
-    args = (Q1, Q2) #Dit wordt dus de data die gefit wordt
-)
-
-x0, y0, a, b, theta = results.x
-
-#Maak een vector van Q1 en Q2:
-vectors = np.column_stack((Q1, Q2))
-centre = np.array([x0, y0])
-squeeze = np.array([a, b])
-R = np.array([[np.cos(theta), - np.sin(theta)], 
-              [np.sin(theta), np.cos(theta)]]).T
-M = squeeze @ R
-#Nu moeten we Q1 en Q2 terug transformeren naar de eenheidscirkel:
-# Verplaatsen naar oorsprong
-centred = vectors - centre
-rotated = centred @ R 
-unit_vectors = rotated / squeeze
-#Test door te plotten:
-
-plt.figure()
-plt.scatter(vectors[:, 0, vectors[:, 1]], s=3)
-plt.scatter(unit_vectors[:, 0], unit_vectors[:, 1], s=5)
-plt.axis("equal")
-plt.grid()
-plt.show()
-
-#####################
 #Totale functie
 def transform(x, y):
     """
@@ -57,8 +22,9 @@ def transform(x, y):
     centre = np.array([x0, y0])
     squeeze = np.array([a, b])
     R = np.array([[np.cos(theta), - np.sin(theta)], 
-                  [np.sin(theta), np.cos(theta)]]).T
+                  [np.sin(theta), np.cos(theta)]])
     centred = vectors - centre
     rotated = centred @ R 
     unit_vectors = rotated / squeeze
     return unit_vectors[:, 0], unit_vectors[:, 1]
+
