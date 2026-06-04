37ccea9ae5572e5972ee55590bd792e0020a95a7
Timo <timoboomsma2007@gmail.com>
2026-06-04 13:57:43 +0200

    Added some temporary code to the Bepaling file and made a Q1 and Q2 file
    I made twoo files: Q1.npy and Q2.npy so we can use np.load("Q1") to quickly acces the list instead of constandly extracting it from the data. This makes testing way easier.
    I also continued working on the test_ellips_fitting file and I wil start the official testing now

--- Bepaling_Q1_Q2_2.py
+++ Bepaling_Q1_Q2_2.py
@@ -36,3 +36,11 @@ Q1_1z, Q2_1z = bepaling_Q1_Q2(PD1_1z, PD2_1z, PD3_1z)
 Q1_2z, Q2_2z = bepaling_Q1_Q2(PD1_2z, PD2_2z, PD3_2z)
 Q1_3z, Q2_3z = bepaling_Q1_Q2(PD1_3z, PD2_3z, PD3_3z)
 
+#sla de eerste 3 op zodat we ze niet steeds hoeven op te halen
+Q1, Q2 = bepaling_Q1_Q2(PD1_1x, PD2_1x, PD3_1x)
+Q1_kopie = Q1.copy()
+Q2_kopie = Q2.copy()
+
+np.save("Q1.npy", Q1_kopie)
+np.save("Q2.npy", Q2_kopie)
+
--- test_ellips_fitting.py
+++ test_ellips_fitting.py
@@ -1,20 +1,12 @@
-from data_extraction_1 import *
-from bepaling_Q1_Q2_2 import *
-from ellips_fitting_and_reshaping_4_5 import *
+#from ellips_fitting_and_reshaping_4_5 import transform
+import numpy as np
 
-data_20260421 = Data_Extract('20260421_HoQIs.txt')
-tijd = data_20260421["T"]
-PD2_1x = data_20260421["SENS_HOQI_1_H_INP_COS_IN"]
-PD3_1x = data_20260421["SENS_HOQI_1_H_INP_MCOS_IN"]
-PD1_1x = data_20260421["SENS_HOQI_1_H_INP_SIN_IN"] 
-
-Q1, Q2 = bepaling_Q1_Q2(PD1_1x, PD2_1x, PD3_1x)
-Q1_kopie = Q1.copy()
-Q2_kopie = Q2.copy()
-print(Q1_kopie)
+Q1 = np.load("Q1.npy")
+Q2 = np.load("Q2.npy")
+print(Q1)
 #plt.figure()
 #plt.scatter(Q1, Q2, s=3)
 #plt.scatter(Q1_new, Q2_new, s=5)
-#plt.axis("equal")
+#plt.axis("equal") 
 #plt.grid()
 #plt.show()
