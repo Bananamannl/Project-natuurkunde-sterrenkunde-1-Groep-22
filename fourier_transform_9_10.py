7f6292dab7b8d6a5bcfb2784d9ebe32a9cf58aa8
KesseDonders <kessedonders@gmail.com>
2026-06-04 15:18:58 +0200

    Update fourier_transform_9_10.py

--- fourier_transform_9_10.py
+++ fourier_transform_9_10.py
@@ -3,6 +3,9 @@ from gwpy.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from data_extraction_1 import *
+from transformatiematrix_8 import displacement_dof
+
+x_lijst, y_lijst, z_lijst, Rx_lijst, Ry_lijst, Rz_lijst = displacement_dof
 
 def get_asd(data, fs, segment_time):
     """
@@ -11,12 +14,13 @@ def get_asd(data, fs, segment_time):
     """
     data = np.asarray(data)
     data = data - np.mean(data) #trek het gemiddelde eraf om te voorkomen dat je een ziek grote piek rond 0 Hz krijgt
-    ts = TimeSeries(data, sample_rate= fs)
+    ts = TimeSeries(data, sample_rate=fs)
     ASD = ts.asd(seconds=segment_time)
     return ASD
 
-#Vervolgens kun je de asd op de volgende manier plotten
-asd = get_asd(lijst, fs=1000, segment_time=10)
+# Vervolgens kun je de asd op de volgende manier plotten
+# Bijvoorbeeld voor de x-lijst:
+asd = get_asd(x_lijst, fs=1000, segment_time=10)
 
 plt.figure()
 plt.loglog(asd.frequencies.value, asd.value)
@@ -24,6 +28,3 @@ plt.xlabel("Frequency [Hz]")
 plt.ylabel("ASD [displacement]")
 plt.grid(True, which="both")
 plt.show()
-
-
-##Testen
