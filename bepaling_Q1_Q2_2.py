f36cad133411e850bfa8486288157e71b68e880f
Timo <timoboomsma2007@gmail.com>
2026-06-04 15:36:48 +0200

    I deleted the code we used for saving
    I made the bepaling file less clutterd by deleting stuff wil don't need anymore, but I saved a bit so we can copy it if we need it later

--- bepaling_Q1_Q2_2.py
+++ bepaling_Q1_Q2_2.py
@@ -40,32 +40,8 @@ def Q_lijsten():
     return Q1_1x, Q2_1x, Q1_2x, Q2_2x, Q1_3x, Q2_3x, Q1_1z, Q2_1z, Q1_2z, Q2_2z, Q1_3z, Q2_3z
 
 #sla de eerste 3 op zodat we ze niet steeds hoeven op te halen
-Q1_2x, Q2_2x = bepaling_Q1_Q2(PD1_2x, PD2_2x, PD3_2x)
-Q1_2x_kopie = Q1_2x.copy()
-Q2_2x_kopie = Q2_2x.copy()
-np.save("2xQ1.npy", Q1_2x_kopie)
-np.save("2xQ2.npy", Q2_2x_kopie)
-
-Q1_3x, Q2_3x = bepaling_Q1_Q2(PD1_3x, PD2_3x, PD3_3x)
-Q1_3x_kopie = Q1_3x.copy()
-Q2_3x_kopie = Q2_3x.copy()
-np.save("3xQ1.npy", Q1_3x_kopie)
-np.save("3xQ2.npy", Q2_3x_kopie)
- 
-Q1_1z, Q2_1z = bepaling_Q1_Q2(PD1_1z, PD2_1z, PD3_1z)
-Q1_1z_kopie = Q1_1z.copy()
-Q2_1z_kopie = Q2_1z.copy()
-np.save("1zQ1.npy", Q1_1z_kopie)
-np.save("1zQ2.npy", Q2_1z_kopie)
-
-Q1_2z, Q2_2z = bepaling_Q1_Q2(PD1_2z, PD2_2z, PD3_2z)
-Q1_2z_kopie = Q1_2z.copy()
-Q2_2z_kopie = Q2_2z.copy()
-np.save("2zQ1.npy", Q1_2z_kopie)
-np.save("2zQ2.npy", Q2_2z_kopie)
-
-Q1_3z, Q2_3z = bepaling_Q1_Q2(PD1_3z, PD2_3z, PD3_3z)
-Q1_3z_kopie = Q1_3z.copy()
-Q2_3z_kopie = Q2_3z.copy()
-np.save("3zQ1.npy", Q1_3z_kopie)
-np.save("3zQ2.npy", Q2_3z_kopie)
+#Q1_2x, Q2_2x = bepaling_Q1_Q2(PD1_2x, PD2_2x, PD3_2x)
+#Q1_2x_kopie = Q1_2x.copy()
+#Q2_2x_kopie = Q2_2x.copy()
+#np.save("2xQ1.npy", Q1_2x_kopie)
+#np.save("2xQ2.npy", Q2_2x_kopie)
