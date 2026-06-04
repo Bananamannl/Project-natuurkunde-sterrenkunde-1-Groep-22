e7dac20b412491dde9d26455ffaa4f4ea9665bdc
KesseDonders <kessedonders@gmail.com>
2026-06-04 16:31:16 +0200

    Fourier code now returns a figure
    The code now gives a figure as an output instead of an error, think that's good? Results aren't very nice yet, but I'll take a look at that. For now, here's the working code

--- fourier_transform_9_10.py
+++ fourier_transform_9_10.py
@@ -13,7 +13,7 @@ def get_asd(data, fs, segment_time):
     data = np.asarray(data)
     data = data - np.mean(data) #trek het gemiddelde eraf om te voorkomen dat je een enorm grote piek rond 0 Hz krijgt
     ts = TimeSeries(data, sample_rate=fs)
-    ASD = ts.asd(seconds=segment_time)
+    ASD = ts.asd(segment_time)
     return ASD
 
 Q1_1x, Q2_1x, Q1_2x, Q2_2x, Q1_3x, Q2_3x, Q1_1z, Q2_1z, Q1_2z, Q2_2z, Q1_3z, Q2_3z = np.load('1xQ1.npy'), np.load('1xQ2.npy'), np.load('2xQ1.npy'), np.load('2xQ2.npy'), np.load('3xQ1.npy'), np.load('3xQ2.npy'), np.load('1zQ1.npy'), np.load('1zQ2.npy'), np.load('2zQ1.npy'), np.load('2zQ2.npy'), np.load('3zQ1.npy'), np.load('3zQ2.npy')
@@ -32,7 +32,7 @@ length_3z_lijst = Q1_Q2_Length(Q1_3z, Q2_3z)
 
 x_lijst, y_lijst, z_lijst, Rx_lijst, Ry_lijst, Rz_lijst  = transformatiematrix(length_1x_lijst, length_2x_lijst, length_3x_lijst, length_1z_lijst, length_2z_lijst, length_3z_lijst)
 
-asd = get_asd(x_lijst, fs=1000, segment_time=10)
+asd = get_asd(x_lijst, fs=1000, segment_time=1000)
 
 # Vervolgens kun je de asd op de volgende manier plotten
 
