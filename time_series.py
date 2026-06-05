76cf22c5384d5805831674315d89d117ef3146d1
Merijn Post <merijnpost2006@gmail.com>
2026-06-05 11:23:27 +0200

    Added the time series code, and adjusted arctan formula
    I have added the code that generates the time series of the different cartesian axes, and adjusted the wavelength so that the arctan function outputs the lenghts in micrometers

---
+++ time_series.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from data_extraction_1 import *
+from bepaling_Q1_Q2_2 import *
+from ellips_fitting_and_reshaping_3_4 import *
+from arctan_Q1_Q2_5_6_7 import *
+from transformatiematrix_8 import *
+
+## First we extract all the data from the HoQI data file:
+
+data_20260421 = Data_Extract('20260421_HoQIs.txt')
+
+## We use the saved Q1 and Q2 lists to import the data
+unfitted_Q1_1x, unfitted_Q2_1x, unfitted_Q1_2x, unfitted_Q2_2x, unfitted_Q1_3x, unfitted_Q2_3x, unfitted_Q1_1z, unfitted_Q2_1z, unfitted_Q1_2z, unfitted_Q2_2z, unfitted_Q1_3z, unfitted_Q2_3z = np.load('1xQ1.npy'), np.load('1xQ2.npy'), np.load('2xQ1.npy'), np.load('2xQ2.npy'), np.load('3xQ1.npy'), np.load('3xQ2.npy'), np.load('1zQ1.npy'), np.load('1zQ2.npy'), np.load('2zQ1.npy'), np.load('2zQ2.npy'), np.load('3zQ1.npy'), np.load('3zQ2.npy')
+
+## We then use the ellips fitting to adjust this data
+fitted_Q1_1x, fitted_Q2_1x = transform(unfitted_Q1_1x, unfitted_Q2_1x)
+fitted_Q1_2x, fitted_Q2_2x = transform(unfitted_Q1_2x, unfitted_Q2_2x)
+fitted_Q1_3x, fitted_Q2_3x = transform(unfitted_Q1_3x, unfitted_Q2_3x) 
+fitted_Q1_1z, fitted_Q2_1z = transform(unfitted_Q1_1z, unfitted_Q2_1z)
+fitted_Q1_2z, fitted_Q2_2z = transform(unfitted_Q1_2z, unfitted_Q2_2z)
+fitted_Q1_3z, fitted_Q2_3z = transform(unfitted_Q1_3z, unfitted_Q2_3z)
+
+## We then use both these data sets and the Q1_Q2_Length function to create the list of HoQI displacements for each
+unfitted_1x, unfitted_2x, unfitted_3x, unfitted_1z, unfitted_2z, unfitted_3z = Q1_Q2_Length(unfitted_Q1_1x, unfitted_Q2_1x), Q1_Q2_Length(unfitted_Q1_2x, unfitted_Q2_2x), Q1_Q2_Length(unfitted_Q1_3x, unfitted_Q2_3x), Q1_Q2_Length(unfitted_Q1_1z, unfitted_Q2_1z), Q1_Q2_Length(unfitted_Q1_2z, unfitted_Q2_2z), Q1_Q2_Length(unfitted_Q1_3z, unfitted_Q2_3z)
+
+fitted_1x, fitted_2x, fitted_3x, fitted_1z, fitted_2z, fitted_3z = Q1_Q2_Length(fitted_Q1_1x, fitted_Q2_1x), Q1_Q2_Length(fitted_Q1_2x, fitted_Q2_2x), Q1_Q2_Length(fitted_Q1_3x, fitted_Q2_3x), Q1_Q2_Length(fitted_Q1_1z, fitted_Q2_1z), Q1_Q2_Length(fitted_Q1_2z, fitted_Q2_2z), Q1_Q2_Length(fitted_Q1_3z, fitted_Q2_3z)
+
+## And then we use the tranformationmatrix to turn these into cartesian coordinates
+unfitted_x, unfitted_y, unfitted_z, unfitted_Rx, unfitted_Ry, unfitted_Rz = transformatiematrix(unfitted_1x, unfitted_2x, unfitted_3x, unfitted_1z, unfitted_2z, unfitted_3z)
+
+fitted_x, fitted_y, fitted_z, fitted_Rx, fitted_Ry, fitted_Rz = transformatiematrix(fitted_1x, fitted_2x, fitted_3x, fitted_1z, fitted_2z, fitted_3z)
+
+## And import the ones from the HoQI file, as well as the time axis:
+time = data_20260421['T']
+
+data_x, data_y, data_z, data_Rx, data_Ry, data_Rz = data_20260421['RM_HOQI_X'], data_20260421['RM_HOQI_Y'], data_20260421['RM_HOQI_Z'], data_20260421['RM_HOQI_RX'], data_20260421['RM_HOQI_RY'], data_20260421['RM_HOQI_RZ']
+
+## And now we plot the timeseries
+fig, axes = plt.subplots(2, 1)
+axes[0,0].set_title('x motion timeseries')
+axes[0,0].set_ylabel('motion')
+axes[0,0].set_xlabel('time')
+axes[0,0].plot(time, unfitted_x, ',')
+plt.show()
