aae61327d499a2cae197f4ae08ed25192c6f7a1b
Timo <timoboomsma2007@gmail.com>
2026-06-09 15:45:55 +0200

    Fixed the changing ellips
    Now we can see how the ellips changes over time

--- Data_Analysis_Part_2/changing_ellips.py
+++ Data_Analysis_Part_2/changing_ellips.py
@@ -1,12 +1,19 @@
 from functies import *
-from ellipse_parameters import *
+from ellipse_parameters import parameters_timeseries
 import matplotlib.pyplot as plt
 
 Q1, Q2 = np.load("Data_Analyse_Dataset_1\\1zQ1.npy"), np.load("Data_Analyse_Dataset_1\\1zQ2.npy")
-Q1, Q2 = transform(Q1[:1430], Q2[:1430])
+Q1, Q2 = transform(Q1[:2860], Q2[:2860])
 
-paramaters = parameters_timeseries(Q1, Q2, window_size=100, step_size=50)
+paramaters = parameters_timeseries(Q1, Q2, window_size=100, step_size=5)
+parameters = paramaters - paramaters.mean(axis=0)
 
-plt.plot(paramaters)
-plt.legend(["x0", "y0", "theta", "a", "b", "area"])
+data = parameters[:, :3] * 100
+
+# plt.plot(paramaters)
+# plt.legend(["x0", "y0", "theta", "a", "b", "area"])
+# plt.show()
+
+plt.plot(data)
+plt.legend(["x0", "y0", "a"])
 plt.show()
--- Data_Analysis_Part_2/ellipse_parameters.py
+++ Data_Analysis_Part_2/ellipse_parameters.py
@@ -5,8 +5,9 @@ def parameters(x, y, start_parameters=None):
     """
     Takes Q1 and Q2 data (np.array's) and spits out the fitting parameters
     the output is in the form of a 6-dim vector:
-    (x0, y0, theta, a, b, area)
+    (x0, y0, a, b, theta, area)
     """
+    
     if start_parameters is None:
         start_parameters = [0, 0, 1, 1, 0]
     results = least_squares(
@@ -19,11 +20,14 @@ def parameters(x, y, start_parameters=None):
         a, b = b, a
         theta += np.pi / 2
     area = np.pi * a * b
-    vector = np.column_stack((x0, y0, theta, a, b, area))
+    vector = np.column_stack((x0, y0, a, b, theta, area))
     start_parameters = [x0, y0, a, b, theta]
     return vector, start_parameters
 
 def parameters_timeseries(x, y, window_size=None, step_size=None):
+    """
+    output: lijst met 6-dim vectoren
+    """
     if window_size is None:
         window_size = 1000
     if step_size is None:
