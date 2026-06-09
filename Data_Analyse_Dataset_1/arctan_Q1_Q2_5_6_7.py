ab7864484318c018b35941b2638df4078e844690
janne-lemmens <jannelemmens07@gmail.com>
2026-06-09 09:57:26 +0200

    Update arctan_Q1_Q2_5_6_7.py
    Verbetering fout door nieuwe functie te definieren en oude functie terug te zetten zodat de rest van de code blijft werken.

--- Data_Analyse_Dataset_1/arctan_Q1_Q2_5_6_7.py
+++ Data_Analyse_Dataset_1/arctan_Q1_Q2_5_6_7.py
@@ -17,6 +17,20 @@ def Q1_Q2_Length(Q1, Q2):
 
     length = (opt_phase * w_length)/(4*np.pi)
 
+    return length
+
+def Q1_Q2_Length_opt_phase_norm_Q(Q1, Q2):
+    """
+    Q1_Q2_Length( np.array, np.array ) - > np.array
+    This function takes two lists of Q1 and Q2 made from the HoQI data, and calculates the optical phase. It then uses the optical phase to calculate the measured length at each timestep. This is then outputed as another np.array.
+    """
+    # The wavelength of the HoQI laser is 1064 nm, adjusted to e-3 to have the outputed length be in um instead
+    w_length = 1064e-3 
+
+    opt_phase = np.unwrap(np.arctan2(Q1, Q2))
+
+    length = (opt_phase * w_length)/(4*np.pi)
+
     Q_absoluut = np.sqrt((Q1)**2 + (Q2)**2)
 
     return length, opt_phase, Q_absoluut
@@ -24,8 +38,7 @@ def Q1_Q2_Length(Q1, Q2):
 # testing the function for one HoQI: 
 HoQI_1_Q1, HoQI_1_Q2 = np.load('Data_Analyse_Dataset_1/3zQ1.npy'), np.load('Data_Analyse_Dataset_1/3zQ2.npy')
 
-displacement_list_HoQI_1, optical_phase_list_HoQI_1, Q_absoluut_list_HoQI_1 = Q1_Q2_Length(HoQI_1_Q1, HoQI_1_Q2)
+displacement_list_HoQI_1, optical_phase_list_HoQI_1, Q_absoluut_list_HoQI_1 = Q1_Q2_Length_opt_phase_norm_Q(HoQI_1_Q1, HoQI_1_Q2)
 
 # print test (if necessary):
 # print(displacement_list_HoQI_1[0]-displacement_list_HoQI_1[1])
-print (optical_phase_list_HoQI_1)
