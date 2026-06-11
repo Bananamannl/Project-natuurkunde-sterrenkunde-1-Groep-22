cc9c58d952e5319ee9e79bd179e0ccc1049c3cc2
Merijn Post <merijnpost2006@gmail.com>
2026-06-11 14:35:56 +0200

    Update windowed_ellipse_fitting.py

--- Data_Analysis_Part_2/windowed_ellipse_fitting.py
+++ Data_Analysis_Part_2/windowed_ellipse_fitting.py
@@ -49,7 +49,7 @@ def variable_step_window_ellipse_fitting(Q1, Q2, window_size, step_size):
         part_Q1 = Q1[start:end]
         part_Q2 = Q2[start:end]
         
-        list_x0, list_y0, list_a, list_b, list_theta = np.transpose(params_matrix[int((start / step_size) - (window_size / step_size) + 1) : int((start / step_size) + (window_size / step_size) - 1), : ])
+        list_x0, list_y0, list_a, list_b, list_theta = np.transpose(params_matrix[int((start / step_size) - (window_size / step_size) + 1) : int((start / step_size) + (window_size / step_size)), : ])
         vectors = np.column_stack((part_Q1, part_Q2))
 
         x0 = np.average(list_x0)
@@ -76,26 +76,25 @@ Q1_list = [np.load('Data_Analysis_Part_1/1xQ1.npy')[:3000000], np.load('Data_Ana
 
 Q2_list = [np.load('Data_Analysis_Part_1/1xQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/2xQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/3xQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/1zQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/2zQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/3zQ2.npy')[:3000000]]
 
-Q1_transformed = [0]*6
-Q2_transformed = [0]*6
-for h in range(0,6):
-    if h <= 2:
-        fitted_Q1, fitted_Q2 = standard_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=1000)
-    else:
-        fitted_Q1, fitted_Q2 = standard_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=100)
-    Q1_transformed[h] = fitted_Q1
-    Q2_transformed[h] = fitted_Q2
+# Q1_transformed = [0]*6
+# Q2_transformed = [0]*6
+# for h in range(0,6):
+#     if h <= 2:
+#         fitted_Q1, fitted_Q2 = standard_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=1000)
+#     else:
+#         fitted_Q1, fitted_Q2 = standard_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=100)
+#     Q1_transformed[h] = fitted_Q1
+#     Q2_transformed[h] = fitted_Q2
 
 Q1_variable_transformed = [0]*6
 Q2_variable_transformed = [0]*6
 for h in range(0,6):
     if h <= 2:
-        fitted_Q1, fitted_Q2 = variable_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=1000, step_size=1)
+        fitted_Q1, fitted_Q2 = variable_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=1000, step_size=1000)
     else:
-        fitted_Q1, fitted_Q2 = variable_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=100, step_size=1)
+        fitted_Q1, fitted_Q2 = variable_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=100, step_size=100)
     Q1_variable_transformed[h] = fitted_Q1
-    Q2_variable_transformed = [0]*6
-[h] = fitted_Q2
+    Q2_variable_transformed[h] = fitted_Q2
 
 names_list = ['1x', '2x', '3x', '1z', '2z', '3z']
 
@@ -112,18 +111,18 @@ names_list = ['1x', '2x', '3x', '1z', '2z', '3z']
 # plt.subplots_adjust(hspace=0.312, wspace=0.4)
 # plt.show()
 
-figure, axes = plt.subplots(2, 3)
-for i in range(0,2):
-    for j in range(0,3):
-        axes[i, j].set_ylabel('Q2')
-        axes[i, j].set_xlabel('Q1')
-        axes[i, j].plot(Q1_transformed[i*3+j], Q2_transformed[i*3+j], ',r')
-        axes[i, j].set_title(names_list[i*3+j])
-        axes[i, j].set_xlim(-1.43, 1.43)
+# figure, axes = plt.subplots(2, 3)
+# for i in range(0,2):
+#     for j in range(0,3):
+#         axes[i, j].set_ylabel('Q2')
+#         axes[i, j].set_xlabel('Q1')
+#         axes[i, j].plot(Q1_transformed[i*3+j], Q2_transformed[i*3+j], ',r')
+#         axes[i, j].set_title(names_list[i*3+j])
+#         axes[i, j].set_xlim(-1.43, 1.43)
 
-figure.tight_layout()
-plt.subplots_adjust(hspace=0.312, wspace=0.4)
-plt.show()
+# figure.tight_layout()
+# plt.subplots_adjust(hspace=0.312, wspace=0.4)
+# plt.show()
 
 figure, axes = plt.subplots(2, 3)
 for i in range(0,2):
