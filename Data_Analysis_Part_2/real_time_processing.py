cb18518539b7d8a5a529df94fb85ba4e3e774e19
Timo <timoboomsma2007@gmail.com>
2026-06-11 13:24:34 +0200

    created the real time processing file

--- Data_Analysis_Part_2/real_time_processing.py
+++ Data_Analysis_Part_2/real_time_processing.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import pyvista as pv
 
 HoQIs, Q1, Q2 = np.load("Data_Analysis_Part_1\HoQI_fitted_six_vct_list.npy"), np.load("Data_Analysis_Part_1\\1zQ1.npy"), np.load("Data_Analysis_Part_1\\1zQ2.npy")
-# Q1, Q2 = transform(Q1[0:10000], Q2[0:10000])
-Q1, Q2 = (Q1[0:10000], Q2[0:10000])
+Q1, Q2 = transform(Q1[0:10000], Q2[0:10000])
+# Q1, Q2 = (Q1[0:10000], Q2[0:10000])
 parameters = parameters_timeseries(Q1, Q2, window_size=100, step_size=1)
 
-a = parameters[:, 4]
+a = parameters[:, 2]
 
 matrix_1z = np.array([
     [-2/3, 1/3, 1/3, 0, 0, 0],
@@ -71,4 +71,44 @@ plotter.add_mesh(
     opacity=0.25,
     color="gray"
 )
+
+
+# -------------------------
+# Fit-oppervlak toevoegen
+# -------------------------
+
+coeffs = np.array([ 8.57334684e-01,  2.75617523e-01, -4.17557290e-03, -1.12518106e-03,
+  1.33102732e-03, -5.72146792e-04, -3.27906403e-05, -6.62192067e-02,
+  2.14643885e-03])
+def fitted_surface(x, y):
+    return (
+        coeffs[0]
+        + coeffs[1] * x
+        + coeffs[2] * y
+        + coeffs[3] * x * y
+        + coeffs[4] * x**2 * y
+        + coeffs[5] * y**2 * x
+        + coeffs[6] * x**2 * y**2
+        + coeffs[7] * x**2
+        + coeffs[8] * y**2
+    )
+
+x_data = HoQI1z_a[:, 0]
+y_data = HoQI1z_a[:, 1]
+
+x_grid = np.linspace(x_data.min(), x_data.max(), 80)
+y_grid = np.linspace(y_data.min(), y_data.max(), 80)
+
+X, Y = np.meshgrid(x_grid, y_grid)
+Z = fitted_surface(X, Y)
+
+surface = pv.StructuredGrid(X, Y, Z)
+
+plotter.add_mesh(
+    surface,
+    opacity=0.45,
+    color="orange",
+    show_edges=False
+)
+
 plotter.show()
