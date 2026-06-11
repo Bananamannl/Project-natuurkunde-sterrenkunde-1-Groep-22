c79342018b9ab5ae9b1f23f747bd82ddaaafdc62
Merijn Post <merijnpost2006@gmail.com>
2026-06-11 16:32:53 +0200

    window fitted ASD plots
    This commit uses the new window fitting method to generate ASD's for comparison with the raw data and previous fitting method for all HoQI's

--- Data_Analysis_Part_2/windowed_ellipse_fitting.py
+++ Data_Analysis_Part_2/windowed_ellipse_fitting.py
@@ -34,7 +34,7 @@ def standard_step_window_ellipse_fitting(Q1, Q2, window_size):
         return_Q1.append(list(transformed_part_Q1))
         return_Q2.append(list(transformed_part_Q2))
 
-    return np.array(return_Q1), np.array(return_Q2)
+    return np.array(return_Q1).flatten(), np.array(return_Q2).flatten()
 
 # Defining function with overlap
 def variable_step_window_ellipse_fitting(Q1, Q2, window_size, step_size):
@@ -69,12 +69,13 @@ def variable_step_window_ellipse_fitting(Q1, Q2, window_size, step_size):
         return_Q1.append(list(transformed_part_Q1))
         return_Q2.append(list(transformed_part_Q2))
 
-    return np.array(return_Q1), np.array(return_Q2)
+    return np.array(return_Q1).flatten(), np.array(return_Q2).flatten()
 
 # Using the function to plot the new circles
-Q1_list = [np.load('Data_Analysis_Part_1/1xQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/2xQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/3xQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/1zQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/2zQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/3zQ1.npy')[:3000000]]
 
-Q2_list = [np.load('Data_Analysis_Part_1/1xQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/2xQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/3xQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/1zQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/2zQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/3zQ2.npy')[:3000000]]
+# Q1_list = [np.load('Data_Analysis_Part_1/1xQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/2xQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/3xQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/1zQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/2zQ1.npy')[:3000000], np.load('Data_Analysis_Part_1/3zQ1.npy')[:3000000]]
+
+# Q2_list = [np.load('Data_Analysis_Part_1/1xQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/2xQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/3xQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/1zQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/2zQ2.npy')[:3000000], np.load('Data_Analysis_Part_1/3zQ2.npy')[:3000000]]
 
 # Q1_transformed = [0]*6
 # Q2_transformed = [0]*6
@@ -86,17 +87,17 @@ Q2_list = [np.load('Data_Analysis_Part_1/1xQ2.npy')[:3000000], np.load('Data_Ana
 #     Q1_transformed[h] = fitted_Q1
 #     Q2_transformed[h] = fitted_Q2
 
-Q1_variable_transformed = [0]*6
-Q2_variable_transformed = [0]*6
-for h in range(0,6):
-    if h <= 2:
-        fitted_Q1, fitted_Q2 = variable_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=1000, step_size=100)
-    else:
-        fitted_Q1, fitted_Q2 = variable_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=100, step_size=10)
-    Q1_variable_transformed[h] = fitted_Q1
-    Q2_variable_transformed[h] = fitted_Q2
+# Q1_variable_transformed = [0]*6
+# Q2_variable_transformed = [0]*6
+# for h in range(0,6):
+#     if h <= 2:
+#         fitted_Q1, fitted_Q2 = variable_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=1000, step_size=100)
+#     else:
+#         fitted_Q1, fitted_Q2 = variable_step_window_ellipse_fitting(Q1_list[h], Q2_list[h], window_size=100, step_size=10)
+#     Q1_variable_transformed[h] = fitted_Q1
+#     Q2_variable_transformed[h] = fitted_Q2
 
-names_list = ['1x', '2x', '3x', '1z', '2z', '3z']
+# names_list = ['1x', '2x', '3x', '1z', '2z', '3z']
 
 # Make a plot of all the ellipses on top of eachother
 # figure, axes = plt.subplots(2, 3)
@@ -116,7 +117,7 @@ names_list = ['1x', '2x', '3x', '1z', '2z', '3z']
 #     for j in range(0,3):
 #         axes[i, j].set_ylabel('Q2')
 #         axes[i, j].set_xlabel('Q1')
-#         axes[i, j].plot(Q1_transformed[i*3+j], Q2_transformed[i*3+j], ',r')
+#         axes[i, j].plot(Q1_transformed[i*3+j], Q2_transformed[i*3+j], ',g')
 #         axes[i, j].set_title(names_list[i*3+j])
 #         axes[i, j].set_xlim(-1.43, 1.43)
 
@@ -124,15 +125,15 @@ names_list = ['1x', '2x', '3x', '1z', '2z', '3z']
 # plt.subplots_adjust(hspace=0.312, wspace=0.4)
 # plt.show()
 
-figure, axes = plt.subplots(2, 3)
-for i in range(0,2):
-    for j in range(0,3):
-        axes[i, j].set_ylabel('Q2')
-        axes[i, j].set_xlabel('Q1')
-        axes[i, j].plot(Q1_variable_transformed[i*3+j], Q2_variable_transformed[i*3+j], ',g')
-        axes[i, j].set_title(names_list[i*3+j])
-        axes[i, j].set_xlim(-1.43, 1.43)
+# figure, axes = plt.subplots(2, 3)
+# for i in range(0,2):
+#     for j in range(0,3):
+#         axes[i, j].set_ylabel('Q2')
+#         axes[i, j].set_xlabel('Q1')
+#         axes[i, j].plot(Q1_variable_transformed[i*3+j], Q2_variable_transformed[i*3+j], ',g')
+#         axes[i, j].set_title(names_list[i*3+j])
+#         axes[i, j].set_xlim(-1.43, 1.43)
 
-figure.tight_layout()
-plt.subplots_adjust(hspace=0.312, wspace=0.4)
-plt.show()
+# figure.tight_layout()
+# plt.subplots_adjust(hspace=0.312, wspace=0.4)
+# plt.show()
