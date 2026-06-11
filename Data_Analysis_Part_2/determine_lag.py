8de8aed323658502ba6bef6d547bad1dd59e1a58
Timo <timoboomsma2007@gmail.com>
2026-06-11 14:17:46 +0200

    Played a little bit more with interpolating

--- Data_Analysis_Part_2/determine_lag.py
+++ Data_Analysis_Part_2/determine_lag.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 
 HoQIs, Q1, Q2 = np.load("Data_Analysis_Part_1\HoQI_fitted_six_vct_list.npy"), np.load("Data_Analysis_Part_1\\1zQ1.npy"), np.load("Data_Analysis_Part_1\\1zQ2.npy")
 # Q1, Q2 = transform(Q1[0:10000], Q2[0:10000])
-Q1, Q2 = (Q1[:700], Q2[:700])
+Q1, Q2 = (Q1[20000:21500], Q2[20000:21500])
 parameters = parameters_timeseries(Q1, Q2, window_size=100, step_size=1) #1401 windows
 
+
 a, b = parameters[:, 0], parameters[:, 1] #dus een lijst met 1401 getallen
-a = (a - np.mean(a)) * 200
-b = (b - np.mean(b)) * 200
+a = (a - np.mean(a)) * 20
+b = (b - np.mean(b)) * 20
 def norm(Q1, Q2):
     """
     A function that finds the norm of every Q vector
@@ -20,10 +21,21 @@ def norm(Q1, Q2):
     return norms
 norms = norm(Q1, Q2)
 
-plt.plot(a, label='a')
-plt.plot(b, label='b')
-plt.plot(norms, label='norms')
-plt.legend(fontsize=12, loc = 'upper right')
-plt.show()
+# plt.plot(lijst, a, label='a')
+# plt.plot(lijst, b, label='b')
+# plt.plot(norms, label='norms')
+# plt.legend(fontsize=12, loc = 'upper right')
+# plt.show()
 
+corr_lijst = []
 #Conclusie, gebruik gewoon lag=0
+for i in range(100):
+    # print("lag=", i, "geeft:")
+    # print(np.corrcoef(norms[i:1401+i], b)[1, 0])
+    corr_lijst.append(np.corrcoef(norms[i:1401+i], b)[1, 0])
+
+hoogste_waarde = max(corr_lijst)
+index = corr_lijst.index(hoogste_waarde)
+
+print("Hoogste waarde:", hoogste_waarde)
+print("Index:", index)
--- Data_Analysis_Part_2/interpolate_fit_2.py
+++ Data_Analysis_Part_2/interpolate_fit_2.py
@@ -4,16 +4,21 @@ from ellipse_parameters import *
 import numpy as np
 import pyvista as pv
 
-HoQIs, Q1, Q2 = np.load("Data_Analysis_Part_1\HoQI_fitted_six_vct_list.npy"), np.load("Data_Analysis_Part_1\\1zQ1.npy"), np.load("Data_Analysis_Part_1\\1zQ2.npy")
-Q1, Q2 = transform(Q1[0:10000], Q2[0:10000])
-# Q1, Q2 = (Q1[0:10000], Q2[0:10000])
+HoQIs, Q1, Q2 = np.load("Data_Analysis_Part_1\HoQI_fitted_six_vct_list.npy"), np.load("Data_Analysis_Part_1\\1xQ1.npy"), np.load("Data_Analysis_Part_1\\1xQ2.npy")
+# Q1, Q2 = transform(Q1[0:10000], Q2[0:10000])
+Q1, Q2 = (Q1[0:10000], Q2[0:10000])
 parameters = parameters_timeseries(Q1, Q2, window_size=100, step_size=1)
 
-a = parameters[:, 2]
+a = parameters[:, 0]
 
 matrix_1z = np.array([
     [-2/3, 1/3, 1/3, 0, 0, 0],
     [0, -np.sqrt(1/3), np.sqrt(1/3), 0, 0, 0]
+])
+
+matrix_1x = np.array([
+    [0, -np.sqrt(1/3), np.sqrt(1/3), 0, 0, 0],
+    [0, 0, 0, 1/3, 1/3, 1/3]
 ])
 
 def orthagonal_displacement_and_parameter(HoQIs, matrix, a):
@@ -25,7 +30,7 @@ def orthagonal_displacement_and_parameter(HoQIs, matrix, a):
     a = (a - np.mean(a)) * 1000
     return np.hstack((vectors, a[:, None]))
 
-points = orthagonal_displacement_and_parameter(HoQIs, matrix_1z, a)
+points = orthagonal_displacement_and_parameter(HoQIs, matrix_1x, a)
 
 # Data
 x_data = points[:, 0]
@@ -39,7 +44,7 @@ rbf = RBFInterpolator(
     xy_data,
     z_data,
     kernel="thin_plate_spline",
-    smoothing=0.01
+    smoothing=0.1
 )
 
 # Grid maken
@@ -71,6 +76,36 @@ plotter.add_mesh(
     render_points_as_spheres=False,
     point_size=5,
     color="lightblue"
+)
+
+max_range = np.max(np.abs(points))
+
+x_axis = pv.Line((-max_range, 0, 0), (max_range, 0, 0))
+y_axis = pv.Line((0, -max_range, 0), (0, max_range, 0))
+z_axis = pv.Line((0, 0, -max_range), (0, 0, max_range))
+
+plotter.add_mesh(x_axis, line_width=3, color="black")
+plotter.add_mesh(y_axis, line_width=3, color="black")
+plotter.add_mesh(z_axis, line_width=3, color="black")
+
+origin = pv.Sphere(
+    radius=max_range * 0.01,
+    center=(0, 0, 0)
+)
+
+plotter.add_mesh(origin, color="red")
+
+plane = pv.Plane(
+    center=(0, 0, 0),
+    direction=(0, 0, 1),
+    i_size=2 * max_range,
+    j_size=2 * max_range
+)
+
+plotter.add_mesh(
+    plane,
+    opacity=0.25,
+    color="gray"
 )
 
 plotter.add_axes(
