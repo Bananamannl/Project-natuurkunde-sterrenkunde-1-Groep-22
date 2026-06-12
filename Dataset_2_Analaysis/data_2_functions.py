f90d682443f5141dfa8d1a02a91d5c00dd678c0b
Merijn Post <merijnpost2006@gmail.com>
2026-06-12 15:00:34 +0200

    More Dataset 2 analysis

--- Dataset_2_Analaysis/data_2_functions.py
+++ Dataset_2_Analaysis/data_2_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import least_squares
 
 def Data_Extract(name):
     """
@@ -26,3 +27,34 @@ def bepaling_Q1_Q2(PD1, PD2, PD3):
     Q1 = np.array(PD1)-np.array(PD2)
     Q2 = np.array(PD1)-np.array(PD3)
     return Q1, Q2
+
+def residuals(params, x, y):
+    x0, y0, a, b, theta = params
+    xp = (x - x0) * np.cos(theta) + (y - y0) * np.sin(theta)
+    yp = - (x - x0) * np.sin(theta) + (y - y0) * np.cos(theta)
+    return xp ** 2 / a ** 2 + yp ** 2 / b ** 2 - 1
+
+# totale functie
+def transform(x, y, start_parameters=None):
+    """
+    Takes two np arrays (Q1, Q1) as input. You can also give starting paramaters by adding: start)parameters=[a, b, c, d, e]. Than the function
+    fits the data to an ellips and transforms the data to be on the unit circle
+    Output is again two np arrays which are the transformed versions of the input arrays
+    """
+    if start_parameters is None:
+        start_parameters = [0, 0, 1, 1, 0]
+    results = least_squares(
+        residuals,
+        x0 = start_parameters,
+        args = (x, y)
+    )
+    x0, y0, a, b, theta = results.x
+    vectors = np.column_stack((x, y))
+    centre = np.array([x0, y0])
+    squeeze = np.array([a, b])
+    R = np.array([[np.cos(theta), - np.sin(theta)], 
+                  [np.sin(theta), np.cos(theta)]])
+    centred = vectors - centre
+    rotated = centred @ R 
+    unit_vectors = rotated / squeeze
+    return unit_vectors[:, 0], unit_vectors[:, 1]
