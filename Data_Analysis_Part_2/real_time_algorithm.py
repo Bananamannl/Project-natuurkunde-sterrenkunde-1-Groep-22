c5a11a1a959cb39d4417f1ba09b7ad3dad42ab6e
Timo <timoboomsma2007@gmail.com>
2026-06-12 17:18:14 +0200

    Started with a file for real time analysis

---
+++ Data_Analysis_Part_2/real_time_algorithm.py
@@ -0,0 +1,30 @@
+import numpy as np
+from knn_functions import *
+from functions import *
+from windowed_ellipse_fitting import standard_step_window_ellipse_fitting
+
+#begin met allenn Q lijsten te importeren
+Q1_1x, Q2_1x = np.load("Data_Analysis_Part_1\\1xQ1.npy"), np.load("Data_Analysis_Part_1\\1xQ2.npy")
+Q1_2x, Q2_2x = np.load("Data_Analysis_Part_1\\2xQ1.npy"), np.load("Data_Analysis_Part_1\\2xQ2.npy")
+Q1_3x, Q2_3x = np.load("Data_Analysis_Part_1\\3xQ1.npy"), np.load("Data_Analysis_Part_1\\3xQ2.npy")
+Q1_1z, Q2_1z = np.load("Data_Analysis_Part_1\\1zQ1.npy"), np.load("Data_Analysis_Part_1\\1zQ2.npy")
+Q1_2z, Q2_2z = np.load("Data_Analysis_Part_1\\2zQ1.npy"), np.load("Data_Analysis_Part_1\\2zQ2.npy")
+Q1_3z, Q2_3z = np.load("Data_Analysis_Part_1\\3zQ1.npy"), np.load("Data_Analysis_Part_1\\3zQ2.npy")
+#10.000 punten parameters fitten
+Q_lijst = [[Q1_1x, Q2_1x], [Q1_2x, Q2_2x], [Q1_3x, Q2_3x], [Q1_1z, Q2_1z], [Q1_2z, Q2_2z], [Q1_3z, Q2_3z]]
+windows = [500, 500, 210, 250, 250, 250]
+initial_training_size = 10500
+
+transformed_Q_lijst = []
+parameters_lijst = []
+for index, Q in enumerate(Q_lijst):
+    Q1 = Q[0]
+    Q2 = Q[1]
+    windows_size = windows[index]
+    Q1_transformed, Q2_transformed = standard_step_window_ellipse_fitting(Q1[:initial_training_size], Q2[:initial_training_size], window_size=windows_size)
+    transformed_Q_lijst.append([Q1_transformed, Q2_transformed])
+    parameters = parameters_timeseries(Q[0][:initial_training_size], Q[1][:initial_training_size], window_size=windows_size, step_size=windows_size)
+    parameters_lijst.append(parameters)
+
+for i in parameters_lijst:
+    print(i.shape)
