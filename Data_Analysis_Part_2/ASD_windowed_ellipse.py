329c0219f66bc2d880f4eb96ebbd551f8b22da52
Merijn Post <merijnpost2006@gmail.com>
2026-06-12 13:06:10 +0200

    Changed window sizes
    Played around with the window sizes on the windowed ellipse fitting code to get better plots.

--- Data_Analysis_Part_2/ASD_windowed_ellipse.py
+++ Data_Analysis_Part_2/ASD_windowed_ellipse.py
@@ -6,12 +6,12 @@ from functions import *
 segment_time_set = 1000
 fs_set = 1000
 
-Q1_list = np.load('Data_Analysis_Part_1/3zQ1.npy')[:3000000]
-Q2_list = np.load('Data_Analysis_Part_1/3zQ2.npy')[:3000000]
+Q1_list = np.load('Data_Analysis_Part_1/2zQ1.npy')[:3000000]
+Q2_list = np.load('Data_Analysis_Part_1/2zQ2.npy')[:3000000]
 
 transformed_Q1, transformed_Q2 = transform(Q1_list, Q2_list)
 
-windowed_transformed_Q1, windowed_transformed_Q2 = standard_step_window_ellipse_fitting(Q1_list, Q2_list, window_size=1000)
+windowed_transformed_Q1, windowed_transformed_Q2 = standard_step_window_ellipse_fitting(Q1_list, Q2_list, window_size=250)
 
 length_3x_lijst = Q1_Q2_Length(Q1_list, Q2_list)
 transformed_length_3x_lijst = Q1_Q2_Length(transformed_Q1, transformed_Q2)
@@ -27,23 +27,21 @@ axes[0].set_xlabel("Frequency (Hz)")
 axes[0].set_ylabel("ASD (um Hz^(-1/2))")
 axes[0].set_xlim(1e-3, 1e3)
 axes[0].grid(True, which="both")
-axes[0].set_title('ASD diagram for the raw data (1z)')
+axes[0].set_title('ASD diagram for the raw data (3z)')
 
 axes[1].loglog(transformed_asd.frequencies.value, transformed_asd, 'r')
 axes[1].set_xlabel("Frequency (Hz)")
 axes[1].set_ylabel("ASD (um Hz^(-1/2))")
 axes[1].set_xlim(1e-3, 1e3)
-axes[1].set_ylim(1e-9, 1e2)
 axes[1].grid(True, which="both")
-axes[1].set_title('ASD diagram for the transformed data (1z)')
+axes[1].set_title('ASD diagram for the transformed data (3z)')
 
 axes[2].loglog(windowed_transformed_asd.frequencies.value, windowed_transformed_asd, 'g')
 axes[2].set_xlabel("Frequency (Hz)")
 axes[2].set_ylabel("ASD (um Hz^(-1/2))")
 axes[2].set_xlim(1e-3, 1e3)
-axes[2].set_ylim(1e-9, 1e2)
 axes[2].grid(True, which="both")
-axes[2].set_title('ASD diagram for the windowed transformed data (1z)')
+axes[2].set_title('ASD diagram for the windowed transformed data (3z)')
 
 plt.subplots_adjust(hspace=0.312)
 plt.show()
