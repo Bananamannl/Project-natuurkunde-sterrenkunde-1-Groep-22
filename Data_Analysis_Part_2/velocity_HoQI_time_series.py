c783a1ff4038ea70267f225c4187d3c4fa38130f
KesseDonders <kessedonders@gmail.com>
2026-06-16 12:10:52 +0200

    Added comments to make the codes more accessible for others
    I also changed some of the axis lables, the plot titles, and the titles of the saved .png files.

--- Data_Analysis_Part_2/velocity_HoQI_time_series.py
+++ Data_Analysis_Part_2/velocity_HoQI_time_series.py
@@ -1,14 +1,18 @@
+# importing the necessary libraries and functions
 import numpy as np
 import matplotlib.pyplot as plt
 
 from functions import *
 
+# introducing the different window sizes
 window_size_1x_2x = 500
 window_size_3x = 210
 window_size_1z_2z_3z = 250
 
+# loading in both Q lists for each of the six HoQI's
 Q1_1x, Q2_1x, Q1_2x, Q2_2x, Q1_3x, Q2_3x, Q1_1z, Q2_1z, Q1_2z, Q2_2z, Q1_3z, Q2_3z = np.load('Data_Analysis_Part_1/1xQ1.npy'), np.load('Data_Analysis_Part_1/1xQ2.npy'), np.load('Data_Analysis_Part_1/2xQ1.npy'), np.load('Data_Analysis_Part_1/2xQ2.npy'), np.load('Data_Analysis_Part_1/3xQ1.npy'), np.load('Data_Analysis_Part_1/3xQ2.npy'), np.load('Data_Analysis_Part_1/1zQ1.npy'), np.load('Data_Analysis_Part_1/1zQ2.npy'), np.load('Data_Analysis_Part_1/2zQ1.npy'), np.load('Data_Analysis_Part_1/2zQ2.npy'), np.load('Data_Analysis_Part_1/3zQ1.npy'), np.load('Data_Analysis_Part_1/3zQ2.npy')
 
+# transforming the Q lists using single ellipse fitting
 Q1_1x_single, Q2_1x_single = transform(Q1_1x, Q2_1x)
 Q1_2x_single, Q2_2x_single = transform(Q1_2x, Q2_2x)
 Q1_3x_single, Q2_3x_single = transform(Q1_3x, Q2_3x)
@@ -16,15 +20,18 @@ Q1_1z_single, Q2_1z_single = transform(Q1_1z, Q2_1z)
 Q1_2z_single, Q2_2z_single = transform(Q1_2z, Q2_2z)
 Q1_3z_single, Q2_3z_single = transform(Q1_3z, Q2_3z)
 
-Q1_1x_windowed, Q2_1x_windowed = standard_step_window_ellipse_fitting(Q1_1x, Q2_1x, window_size_1x_2x)
-Q1_2x_windowed, Q2_2x_windowed = standard_step_window_ellipse_fitting(Q1_2x, Q2_2x, window_size_1x_2x)
-Q1_3x_windowed, Q2_3x_windowed = standard_step_window_ellipse_fitting(Q1_3x, Q2_3x, window_size_3x)
-Q1_1z_windowed, Q2_1z_windowed = standard_step_window_ellipse_fitting(Q1_1z, Q2_1z, window_size_1z_2z_3z)
-Q1_2z_windowed, Q2_2z_windowed = standard_step_window_ellipse_fitting(Q1_2z, Q2_2z, window_size_1z_2z_3z)
-Q1_3z_windowed, Q2_3z_windowed = standard_step_window_ellipse_fitting(Q1_3z, Q2_3z, window_size_1z_2z_3z)
+# transforming the Q lists using windowed ellipse fitting with step_size = window_size
+# Q1_1x_windowed, Q2_1x_windowed = standard_step_window_ellipse_fitting(Q1_1x, Q2_1x, window_size_1x_2x)
+# Q1_2x_windowed, Q2_2x_windowed = standard_step_window_ellipse_fitting(Q1_2x, Q2_2x, window_size_1x_2x)
+# Q1_3x_windowed, Q2_3x_windowed = standard_step_window_ellipse_fitting(Q1_3x, Q2_3x, window_size_3x)
+# Q1_1z_windowed, Q2_1z_windowed = standard_step_window_ellipse_fitting(Q1_1z, Q2_1z, window_size_1z_2z_3z)
+# Q1_2z_windowed, Q2_2z_windowed = standard_step_window_ellipse_fitting(Q1_2z, Q2_2z, window_size_1z_2z_3z)
+# Q1_3z_windowed, Q2_3z_windowed = standard_step_window_ellipse_fitting(Q1_3z, Q2_3z, window_size_1z_2z_3z)
 
+# loading the six vector with the direct HoQI displacements
 HoQIs = np.load("Data_Analysis_Part_1\\HoQI_fitted_six_vct_list.npy")
 
+# calculating the direct HoQI displacements for the non-fitted Q lists
 # length_1x_list_non_fitted = -Q1_Q2_Length(Q1_1x, Q2_1x)
 # length_2x_list_non_fitted = Q1_Q2_Length(Q1_2x, Q2_2x)
 # length_3x_list_non_fitted = Q1_Q2_Length(Q1_3x, Q2_3x)
@@ -32,6 +39,7 @@ HoQIs = np.load("Data_Analysis_Part_1\\HoQI_fitted_six_vct_list.npy")
 # length_2z_list_non_fitted = -Q1_Q2_Length(Q1_2z, Q2_2z)
 # length_3z_list_non_fitted = -Q1_Q2_Length(Q1_3z, Q2_3z)
 
+# extracting the direct HoQI displacements for the single ellipse fitted Q lists, using the loaded six vector
 length_1x_list_single = HoQIs[0:int(3e6), 0]
 length_2x_list_single = HoQIs[0:int(3e6), 1]
 length_3x_list_single = HoQIs[0:int(3e6), 2]
@@ -39,21 +47,25 @@ length_1z_list_single = HoQIs[0:int(3e6), 3]
 length_2z_list_single = HoQIs[0:int(3e6), 4]
 length_3z_list_single = HoQIs[0:int(3e6), 5]
 
-length_1x_list_windowed = -Q1_Q2_Length(Q1_1x_windowed, Q2_1x_windowed)[:-150]
-length_2x_list_windowed = Q1_Q2_Length(Q1_2x_windowed, Q2_2x_windowed)[:-150]
-length_3x_list_windowed = Q1_Q2_Length(Q1_3x_windowed, Q2_3x_windowed)
-length_1z_list_windowed = -Q1_Q2_Length(Q1_1z_windowed, Q2_1z_windowed)[:-150]
-length_2z_list_windowed = -Q1_Q2_Length(Q1_2z_windowed, Q2_2z_windowed)[:-150]
-length_3z_list_windowed = -Q1_Q2_Length(Q1_3z_windowed, Q2_3z_windowed)[:-150]
+# calculating the direct HoQI displacements for the windowed ellipse fitted Q lists, using step_size = window_size
+# length_1x_list_windowed = -Q1_Q2_Length(Q1_1x_windowed, Q2_1x_windowed)[:-150]
+# length_2x_list_windowed = Q1_Q2_Length(Q1_2x_windowed, Q2_2x_windowed)[:-150]
+# length_3x_list_windowed = Q1_Q2_Length(Q1_3x_windowed, Q2_3x_windowed)
+# length_1z_list_windowed = -Q1_Q2_Length(Q1_1z_windowed, Q2_1z_windowed)[:-150]
+# length_2z_list_windowed = -Q1_Q2_Length(Q1_2z_windowed, Q2_2z_windowed)[:-150]
+# length_3z_list_windowed = -Q1_Q2_Length(Q1_3z_windowed, Q2_3z_windowed)[:-150]
 
+# defining the time list for the velocity time series (the velocity lists will have 2,999,999 data points)
 time_velocity = []
 for i in range(2999999):
     time_velocity.append(i)
 
+# defining the time list for the velocity time series of the windowed ellipse fitted data (the velocity lists of this data will have 150 data points less than the abovementioned, since 3,000,000 mod 210 ≡ 150)
 time_velocity_windowed = []
 for i in range(2999849):
     time_velocity_windowed.append(i)
 
+# defining and calculating the velocity lists for the non-fitted data
 # velocity_1x_non_fitted, velocity_2x_non_fitted, velocity_3x_non_fitted, velocity_1z_non_fitted, velocity_2z_non_fitted, velocity_3z_non_fitted = [[] for j in range(6)]
 
 # for i in range(0, len(length_1x_list_non_fitted)-1):
@@ -71,6 +83,7 @@ for i in range(2999849):
 #     velocity_2z_non_fitted.append(v_2z_non_fitted)
 #     velocity_3z_non_fitted.append(v_3z_non_fitted)
 
+# defining and calculating the velocity lists for the single ellipse fitted data
 velocity_1x_single, velocity_2x_single, velocity_3x_single, velocity_1z_single, velocity_2z_single, velocity_3z_single = [[] for j in range(6)]
 
 for i in range(0, len(length_1x_list_single)-1):
@@ -88,6 +101,7 @@ for i in range(0, len(length_1x_list_single)-1):
     velocity_2z_single.append(v_2z_single)
     velocity_3z_single.append(v_3z_single)
 
+# defining and calculating the velocity lists for the windowed ellipse fitted data
 # velocity_1x_windowed, velocity_2x_windowed, velocity_3x_windowed, velocity_1z_windowed, velocity_2z_windowed, velocity_3z_windowed = [[] for j in range(6)]
 
 # for i in range(0, len(length_1x_list_windowed)-1):
@@ -105,6 +119,7 @@ for i in range(0, len(length_1x_list_single)-1):
 #     velocity_2z_windowed.append(v_2z_windowed)
 #     velocity_3z_windowed.append(v_3z_windowed)
 
+# creating a figure in which the six velocity time series of the non-fitted data will be plotted
 # figure, axes = plt.subplots(2, 3, figsize=(18, 10))
 
 # axes[0, 0].plot(time_velocity, velocity_1x_non_fitted)
@@ -133,6 +148,7 @@ for i in range(0, len(length_1x_list_single)-1):
 # plt.savefig('velocity_non_fitted_time_series.png')
 # plt.show()
 
+# creating a figure in which the six velocity time series of the single ellipse fitted data will be plotted
 figure, axes = plt.subplots(2, 3, figsize=(18, 10))
 
 axes[0, 0].plot(time_velocity, velocity_1x_single)
@@ -161,6 +177,7 @@ plt.tight_layout(w_pad=5.0)
 plt.savefig('velocity_single_time_series.png')
 plt.show()
 
+# creating a figure in which the six velocity time series of the windowed ellipse fitted data will be plotted
 # figure, axes = plt.subplots(2, 3, figsize=(18, 10))
 
 # axes[0, 0].plot(time_velocity_windowed, velocity_1x_windowed)
@@ -189,35 +206,44 @@ plt.show()
 # plt.savefig('velocity_windowed_time_series.png')
 # plt.show()
 
+# defining a time list for the Q1_Q2_3D plots (the position (and Q) lists contain all 3,000,000 data points)
 time = []
 for i in range(3000000):
     time.append(i)
 
+# defining a time list for the Q1_Q2_3D plots of the windowed ellipse fitted data (once again, the position (and Q) lists of this data will have 150 data points less than the abovementioned, since 3,000,000 mod 210 ≡ 150))
 time_windowed = []
 for i in range(2999850):
     time_windowed.append(i)
 
-# at each run, change this accordingly to select the desired HoQI
+# assigning the Q lists that will be used for the 3D plot 
+# at each run, change this accordingly to select the desired HoQI and the desired fitting method
 # Q1, Q2 = Q1_1x, Q2_1x
-# Q1, Q2 = Q1_1x_single, Q2_1x_single
-Q1, Q2 = Q1_1x_windowed, Q2_1x_windowed
+Q1, Q2 = Q1_1z_single, Q2_1z_single
+# Q1, Q2 = Q1_1z_windowed, Q2_1z_windowed
 
-# velocity = velocity_1x_windowed
-title = '1x (windowed)'
+# at each run, change this accordingly to elect the desired title, based on the desired HoQI and the desired fitting method
+title = '1z (single)'
 
-t_start = int(1.625e6)
-t_end = int(1.628e6)
+# at each run, change this accordingly to select the desired time interval
+t_start = int(1.490e6)
+t_end = int(1.496e6)
 
+# creating a figure for the Q1_Q2_3D plot
 figure = plt.figure()
 ax = figure.add_subplot(projection='3d')
+
+# at each run, change this accordingly to select the desired variables that will be shown in the plot
 # ax.plot(Q1[t_start:t_end], Q2[t_start:t_end], velocity[t_start:t_end], lw=0.8, alpha=0.8)
 # ax.plot(Q1[t_start:t_end], Q2[t_start:t_end], time[t_start:t_end], lw=0.8, alpha=0.8)
 ax.plot(Q1[t_start:t_end], Q2[t_start:t_end], time_windowed[t_start:t_end], lw=0.8, alpha=0.8)
 ax.set_xlabel('Q1')
 ax.set_ylabel('Q2')
+
+# at each run, change this accordingly to select the desired z label, based on the choices made beforehand
 # ax.set_zlabel('velocity (um s$^{-1}$)')
 ax.set_zlabel('time (ms)')
 ax.zaxis.labelpad = 20
 ax.set_title(f'HoQI {title}')
-plt.savefig('Q1_Q2_3D_cilinder.png')
+plt.savefig('Q1_Q2_3D_Highv_Lowx.png')
 plt.show()
--- Data_Analysis_Part_2/windowed_dof_displacements.py
+++ Data_Analysis_Part_2/windowed_dof_displacements.py
@@ -1,14 +1,18 @@
+# importing the necessary libraries and functions
 import numpy as np
 import matplotlib.pyplot as plt
 
 from functions import *
 
+# introducing the different window sizes
 window_size_1x_2x = 500
 window_size_3x = 210
 window_size_1z_2z_3z = 250
 
+# loading in both Q lists for each of the six HoQI's
 Q1_1x, Q2_1x, Q1_2x, Q2_2x, Q1_3x, Q2_3x, Q1_1z, Q2_1z, Q1_2z, Q2_2z, Q1_3z, Q2_3z = np.load('Data_Analysis_Part_1/1xQ1.npy'), np.load('Data_Analysis_Part_1/1xQ2.npy'), np.load('Data_Analysis_Part_1/2xQ1.npy'), np.load('Data_Analysis_Part_1/2xQ2.npy'), np.load('Data_Analysis_Part_1/3xQ1.npy'), np.load('Data_Analysis_Part_1/3xQ2.npy'), np.load('Data_Analysis_Part_1/1zQ1.npy'), np.load('Data_Analysis_Part_1/1zQ2.npy'), np.load('Data_Analysis_Part_1/2zQ1.npy'), np.load('Data_Analysis_Part_1/2zQ2.npy'), np.load('Data_Analysis_Part_1/3zQ1.npy'), np.load('Data_Analysis_Part_1/3zQ2.npy')
 
+# transforming the Q lists using windowed ellipse fitting with step_size = window_size
 Q1_1x_windowed, Q2_1x_windowed = standard_step_window_ellipse_fitting(Q1_1x, Q2_1x, window_size_1x_2x)
 Q1_2x_windowed, Q2_2x_windowed = standard_step_window_ellipse_fitting(Q1_2x, Q2_2x, window_size_1x_2x)
 Q1_3x_windowed, Q2_3x_windowed = standard_step_window_ellipse_fitting(Q1_3x, Q2_3x, window_size_3x)
@@ -16,6 +20,7 @@ Q1_1z_windowed, Q2_1z_windowed = standard_step_window_ellipse_fitting(Q1_1z, Q2_
 Q1_2z_windowed, Q2_2z_windowed = standard_step_window_ellipse_fitting(Q1_2z, Q2_2z, window_size_1z_2z_3z)
 Q1_3z_windowed, Q2_3z_windowed = standard_step_window_ellipse_fitting(Q1_3z, Q2_3z, window_size_1z_2z_3z)
 
+# calculating the direct HoQI displacements
 length_1x_list_windowed = -Q1_Q2_Length(Q1_1x_windowed, Q2_1x_windowed)[:-150]
 length_2x_list_windowed = Q1_Q2_Length(Q1_2x_windowed, Q2_2x_windowed)[:-150]
 length_3x_list_windowed = Q1_Q2_Length(Q1_3x_windowed, Q2_3x_windowed)
@@ -23,63 +28,67 @@ length_1z_list_windowed = -Q1_Q2_Length(Q1_1z_windowed, Q2_1z_windowed)[:-150]
 length_2z_list_windowed = -Q1_Q2_Length(Q1_2z_windowed, Q2_2z_windowed)[:-150]
 length_3z_list_windowed = -Q1_Q2_Length(Q1_3z_windowed, Q2_3z_windowed)[:-150]
 
+# calculating the displacements in all six degrees of freedom using the 6x6 transformation matrix
 x_list_windowed, y_list_windowed, z_list_windowed, Rx_list_windowed, Ry_list_windowed, Rz_list_windowed  = transformatiematrix(length_1x_list_windowed, length_2x_list_windowed, length_3x_list_windowed, length_1z_list_windowed, length_2z_list_windowed, length_3z_list_windowed)
 
+# defining a time list for the displacement time series of the windowed ellipse fitted data (there are in total 3,000,000 data points per HoQI, but the last 150 are removed from the data set, since 3,000,000 mod 210 ≡ 150)
 time = []
 for i in range(0, 2999850):
     time.append(i)
 
+# creating a figure in which the six time series of the direct HoQI displacements, one for each HoQI, will be plotted 
 figure, axes = plt.subplots(2, 3, figsize=(18, 10))
 
 axes[0, 0].plot(time, length_1x_list_windowed)
-axes[0, 0].set_title('HoQI positon (1x)')
+axes[0, 0].set_title('HoQI positon (1x) (windowed)')
 
 axes[0, 1].plot(time, length_2x_list_windowed)
-axes[0, 1].set_title('HoQI positon (2x)')
+axes[0, 1].set_title('HoQI positon (2x) (windowed)')
 
 axes[0, 2].plot(time, length_3x_list_windowed)
-axes[0, 2].set_title('HoQI positon (3x)')
+axes[0, 2].set_title('HoQI positon (3x) (windowed)')
 
 axes[1, 0].plot(time, length_1z_list_windowed)
-axes[1, 0].set_title('HoQI positon (1z)')
+axes[1, 0].set_title('HoQI positon (1z) (windowed)')
 
 axes[1, 1].plot(time, length_2z_list_windowed)
-axes[1, 1].set_title('HoQI positon (2z)')
+axes[1, 1].set_title('HoQI positon (2z) (windowed)')
 
 axes[1, 2].plot(time, length_3z_list_windowed)
-axes[1, 2].set_title('HoQI positon (3z)')
+axes[1, 2].set_title('HoQI positon (3z) (windowed)')
 
 for ax in axes.flat:
     ax.set_xlabel('time (ms)')
-    ax.set_ylabel('HoQI positon (um s$^{-1}$)')
+    ax.set_ylabel('positon (um)')
 
 plt.tight_layout(w_pad=5.0)
 plt.savefig('windowed_HoQI_postion_time_series.png')
 plt.show()
 
+# doing the same thing for the displacements in all six degrees of freedom
 figure, axes = plt.subplots(2, 3, figsize=(18, 10))
 
 axes[0, 0].plot(time, x_list_windowed)
-axes[0, 0].set_title('DoF positon (x)')
+axes[0, 0].set_title('DoF positon (x) (windowed)')
 
 axes[0, 1].plot(time, y_list_windowed)
-axes[0, 1].set_title('DoF positon (y)')
+axes[0, 1].set_title('DoF positon (y) (windowed)')
 
 axes[0, 2].plot(time, z_list_windowed)
-axes[0, 2].set_title('DoF positon (z)')
+axes[0, 2].set_title('DoF positon (z) (windowed)')
 
 axes[1, 0].plot(time, Rx_list_windowed)
-axes[1, 0].set_title('DoF positon (Rx)')
+axes[1, 0].set_title('DoF positon (Rx) (windowed)')
 
 axes[1, 1].plot(time, Ry_list_windowed)
-axes[1, 1].set_title('DoF positon (Ry)')
+axes[1, 1].set_title('DoF positon (Ry) (windowed)')
 
 axes[1, 2].plot(time, Rz_list_windowed)
-axes[1, 2].set_title('DoF positon (Rz)')
+axes[1, 2].set_title('DoF positon (Rz) (windowed)')
 
 for ax in axes.flat:
     ax.set_xlabel('time (ms)')
-    ax.set_ylabel('HoQI positon (um s$^{-1}$)')
+    ax.set_ylabel('positon (um)')
 
 plt.tight_layout(w_pad=5.0)
 plt.savefig('windowed_DoF_position_time_series.png')
