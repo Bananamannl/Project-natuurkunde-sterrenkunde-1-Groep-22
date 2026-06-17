aa8122ce53a3148d5ee8c9816d1f154db8868cd3
Timo <timoboomsma2007@gmail.com>
2026-06-17 20:50:06 +0200

    I tried animating the setup

--- Animations/first_try_setup.py
+++ Animations/first_try_setup.py
@@ -1,5 +1,6 @@
 from manim import *
 import numpy as np
+from mass import *
 
 class CylinderScene(ThreeDScene):
     def cylinder_between(self, start, end, radius=0.12, color=GREY):
@@ -19,70 +20,85 @@ class CylinderScene(ThreeDScene):
         cyl.set_fill(color, opacity=1)
         cyl.set_stroke(color, width=1)
 
+
         return cyl
 
     def construct(self):
         self.set_camera_orientation(
-            phi=70 * DEGREES,
+            phi=65 * DEGREES,
             theta=45 * DEGREES
         )
 
-        # =========================
-        # Grote platte basiscilinder
-        # =========================
         base_radius = 3
         base_height = 0.5
 
         base = Cylinder(
             radius=base_radius,
             height=base_height,
-            direction=UP,
+            direction=OUT,   # base ligt in x-y vlak
             resolution=32
         )
-        base.set_fill(BLUE_D, opacity=0.8)
+        base.set_fill(GRAY, opacity=0.8)
         base.set_stroke(WHITE, width=1)
 
-        # =========================
-        # Klein cilindertje bovenaan
-        # =========================
+        z_base_top = base_height / 2
+
         top_radius = 0.25
-        top_height = 0.35
+        top_height = 0.15
+
+        top_center = np.array([0, 0, 2.6])
 
         top_cylinder = Cylinder(
             radius=top_radius,
             height=top_height,
-            direction=UP,
+            direction=OUT,
             resolution=24
         )
-        top_cylinder.set_fill(GREY_BROWN, opacity=1)
+        top_cylinder.set_fill(GRAY, opacity=1)
         top_cylinder.set_stroke(WHITE, width=1)
-
-        # Plaats van het kleine cilindertje
-        top_center = np.array([0, 2.4, 0])
         top_cylinder.move_to(top_center)
 
-        # Onderkant van het kleine cilindertje:
-        top_bottom = top_center + np.array([0, -top_height / 2, 0])
+        top_bottom = top_center + np.array([0, 0, -top_height / 2])
 
-        # =========================
-        # 3 schuine armen
-        # Punten op de bovenkant van de basis
-        # =========================
-        y_base_top = base_height / 2
+        cable_start = top_bottom
+        cable_end = np.array([0, 0, 0.7])
 
-        arm_start_1 = np.array([-1.7, y_base_top, -0.8])
-        arm_start_2 = np.array([0.0,  y_base_top,  1.8])
-        arm_start_3 = np.array([1.7,  y_base_top, -0.8])
+        cable = self.cylinder_between(
+            cable_start,
+            cable_end,
+            radius=0.02,
+            color=WHITE
+        )
+
+        arm_start_1 = np.array([-1.7, -1.0, z_base_top])
+        arm_start_2 = np.array([0.0,   1.8, z_base_top])
+        arm_start_3 = np.array([1.7,  -1.0, z_base_top])
 
         arm1 = self.cylinder_between(arm_start_1, top_bottom, radius=0.12, color=GREY)
         arm2 = self.cylinder_between(arm_start_2, top_bottom, radius=0.12, color=GREY)
         arm3 = self.cylinder_between(arm_start_3, top_bottom, radius=0.12, color=GREY)
 
-        arms = VGroup(arm1, arm2, arm3)
+        setup = VGroup(arm1, arm2, arm3, cable, top_cylinder)
 
-        # =========================
-        # Animatie
-        # =========================
+        m = mass(0.8)
+        m.move_to(cable_end)
+
+        all_objects = VGroup(base, setup, m)
+        all_objects.rotate(20 * DEGREES, axis=OUT, about_point=ORIGIN)
+
         self.play(Create(base))
-        self.play(Create(arms), Create(top_cylinder))
+        self.play(Create(setup))
+        self.play(FadeIn(m))
+
+        self.play(
+            Rotate(
+                all_objects,
+                angle=360 * DEGREES,
+                axis=OUT,          # z-as
+                about_point=ORIGIN
+            ),
+            run_time=2,
+            rate_func=linear
+        )
+
         self.wait(2)
