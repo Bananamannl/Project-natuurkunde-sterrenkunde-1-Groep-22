6f019f02084dbca8c4c3f485b10d7f25fbdc547e
Timo <timoboomsma2007@gmail.com>
2026-06-17 18:11:04 +0200

    Plotting some different stuff

--- Data_Analysis_Part_2/parameters-plot_test.py
+++ Data_Analysis_Part_2/parameters-plot_test.py
@@ -1,24 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from find_windows_interactive import *
 from ellipse_parameters import *
 
-Q1, Q2  = np.load(r"C:\Users\timob\OneDrive - UvA\Project 1\GitHub Map\Project-natuurkunde-sterrenkunde-1-Groep-22\Data_Analysis_Part_1\1zQ1.npy"), np.load(r"C:\Users\timob\OneDrive - UvA\Project 1\GitHub Map\Project-natuurkunde-sterrenkunde-1-Groep-22\Data_Analysis_Part_1\1zQ2.npy")
+Q1, Q2  = np.load(r"C:\Users\timob\OneDrive - UvA\Project 1\GitHub Map\Project-natuurkunde-sterrenkunde-1-Groep-22\Data_Analysis_Part_1\1xQ1.npy"), np.load(r"C:\Users\timob\OneDrive - UvA\Project 1\GitHub Map\Project-natuurkunde-sterrenkunde-1-Groep-22\Data_Analysis_Part_1\1xQ2.npy")
 parms_1z = np.load(r"C:\Users\timob\OneDrive - UvA\Project 1\GitHub Map\Project-natuurkunde-sterrenkunde-1-Groep-22\param_timeseries_1z_step_size_10_window_size_500.npy")
 max_x, max_y, max_a, max_b, max_theta = np.max(parms_1z, axis=0)
 a = parms_1z[:, 2]
 
 print(max_x, max_y, max_a, max_b, max_theta)
 
-Q1, Q2 = Q1[616968:617218], Q2[616968:617218]
+Q1, Q2 = Q1[:1829000], Q2[:1829000]
 
-params = parameters(Q1, Q2)[0]
-x0 = params[:, 0]
-y0 = params[:, 1]
-a = params[:, 2]
-b = params[:, 3]
-theta = params[:, 4]
-# print("vector:", np.hstack(x0, y0, a, b, theta))
-# plt.plot(a)
+params = parameters_timeseries_interactive(Q1, Q2, window_size=250, step_size=250)
+x0 = params[7314, 0]
+y0 = params[7314, 1]
+a = params[7314, 2]
+b = params[7314, 3]
+theta = params[7314, 4]
+
+
+Q1, Q2 = Q1[1828500:1828750], Q2[1828500:1828750]
 
 t = np.linspace(0, 2*np.pi, 500)
 
@@ -35,8 +37,8 @@ a, b = parms_1z[:, 2], parms_1z[:, 3]
 
 # plt.plot(a)
 # plt.plot(b)
-# plt.plot(xr, yr)
-# plt.axis("equal")
+plt.plot(xr, yr)
+plt.axis("equal")
 plt.scatter(Q1, Q2)
 plt.show()
 
