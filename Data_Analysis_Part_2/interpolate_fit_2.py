5aa10ffb219dbc43f77cf04dcf2caa79df30571c
Timo <timoboomsma2007@gmail.com>
2026-06-23 12:57:49 +0200

    Interpolate plot

--- Data_Analysis_Part_2/interpolate_fit_2.py
+++ Data_Analysis_Part_2/interpolate_fit_2.py
@@ -10,7 +10,8 @@ import pyvista as pv
 parameters = np.load(r"C:\Users\timob\OneDrive - UvA\Project 1\GitHub Map\Project-natuurkunde-sterrenkunde-1-Groep-22\param_timeseries_1z_step_size_10_window_size_500.npy")
 HoQIs = np.load("Data_Analysis_Part_1\HoQI_fitted_six_vct_list.npy")
 
-a = parameters[:, 2]  # niet [:, 0]
+print(parameters.shape)
+a = parameters[:, 2] 
 
 # matrix_1z = np.array([
 #     [-2/3, 1/3, 1/3, 0, 0, 0],
@@ -23,6 +24,10 @@ matrix_1z = np.array([
     [0, -np.sqrt(1/3), np.sqrt(1/3), 0, 0, 0]
 ])
 
+matrix_1z = np.array([
+    [1, 0, 0, 0, 0, 0],
+    [0, -np.sqrt(1/3), np.sqrt(1/3), 0, 0, 0]])
+
 matrix_1x = np.array([
     [0, -np.sqrt(1/3), np.sqrt(1/3), 0, 0, 0],
     [0, 0, 0, 1/3, 1/3, 1/3]
@@ -34,6 +39,7 @@ def orthagonal_displacement_and_parameter(HoQIs, matrix, a):
     The function's output is a (3.000.000, 3) np array that has the ortogonal positions in row 1 and 2 and the norm of every data point in row 3
     """
     vectors = HoQIs[:9901] @ matrix.T
+    a = a[:9901]
     a = (a - np.mean(a)) * 1000
     return np.hstack((vectors, a[:, None]))
 
@@ -46,37 +52,37 @@ z_data = points[:, 2]
 
 xy_data = np.column_stack((x_data, y_data))
 
-# # RBF fit
-# rbf = RBFInterpolator(
-#     xy_data,
-#     z_data,
-#     kernel="thin_plate_spline",
-#     smoothing=0.1
-# )
+# RBF fit
+rbf = RBFInterpolator(
+    xy_data,
+    z_data,
+    kernel="thin_plate_spline",
+    smoothing=0.1
+)
 
-# # Grid maken
-# x_grid = np.linspace(x_data.min(), x_data.max(), 100)
-# y_grid = np.linspace(y_data.min(), y_data.max(), 100)
+# Grid maken
+x_grid = np.linspace(x_data.min(), x_data.max(), 100)
+y_grid = np.linspace(y_data.min(), y_data.max(), 100)
 
-# X, Y = np.meshgrid(x_grid, y_grid)
-# xy_grid = np.column_stack((X.ravel(), Y.ravel()))
+X, Y = np.meshgrid(x_grid, y_grid)
+xy_grid = np.column_stack((X.ravel(), Y.ravel()))
 
-# Z = rbf(xy_grid).reshape(X.shape)
+Z = rbf(xy_grid).reshape(X.shape)
 
-# # Surface maken
-# surface = pv.StructuredGrid(X, Y, Z)
+# Surface maken 
+surface = pv.StructuredGrid(X, Y, Z)
 
 # Originele datapunten
 cloud = pv.PolyData(points)
 
 plotter = pv.Plotter()
 
-# plotter.add_mesh(
-#     surface,
-#     opacity=0.45,
-#     color="orange",
-#     show_edges=False
-# )
+plotter.add_mesh(
+    surface,
+    opacity=0.45,
+    color="orange",
+    show_edges=False
+)
 
 plotter.add_mesh(
     cloud,
